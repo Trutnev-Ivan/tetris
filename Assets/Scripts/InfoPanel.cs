b6baba36838bfddbf77d52221383826a47ff1cc7
Иван Трутнев <trutnev@intervolga.ru>
2024-07-14 21:12:42 +0300

    Изменил размер панели с очками

--- Assets/Scripts/InfoPanel.cs
+++ Assets/Scripts/InfoPanel.cs
@@ -4,7 +4,7 @@ public class InfoPanel : MonoBehaviour
 {
     void Start()
     {
-        transform.position = new Vector3(Settings.instance.getWidth() + Settings.instance.getOffset() * 5, 0, 100);
-        transform.localScale = new Vector3(Settings.instance.getWidth() / 2, Settings.instance.getHeight() / 2, 0);
+        transform.position = new Vector3(Settings.instance.getWidth() + Settings.instance.getOffset() * 5, Settings.instance.getHeight() / 4, 100);
+        transform.localScale = new Vector3(Settings.instance.getWidth() / 2, Settings.instance.getHeight() / 4, 0);
     }
 }
